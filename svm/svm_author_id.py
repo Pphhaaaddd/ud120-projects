174acc86a77c733dab9746772d4d01e5bf1b9c77
Suhail Ahamed <suhail511@gmail.com>
2017-11-07 18:54:08 +0400

    play with gamma values

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,37 +19,37 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
-clf = SVC(kernel="rbf", C=10000.0)
+logScale = (2.0**exp for exp in range(0, 20))
 
-# # Reduce training data size by 100
-# features_train = features_train[:len(features_train)/100]
-# labels_train = labels_train[:len(labels_train)/100]
+for i in logScale:
+    gamma = i/10.0
+    C = 5
+    clf = SVC(kernel="rbf", C=C, gamma=gamma)
 
+    # # Reduce training data size by 100
 
-t0 = time()
-clf.fit(features_train,labels_train)
-print "Training time: ", round(time() - t0,3) ,"s"
+    t0 = time()
+    clf.fit(features_train,labels_train)
+    #print "Training time: ", round(time() - t0,3) ,"s"
 
+    t0 = time()
+    pred = clf.predict(features_test)
+    #print "Prediction time: ", round(time() - t0,3) , "s"
 
-t0 = time()
-pred = clf.predict(features_test)
-print "Prediction time: ", round(time() - t0,3) , "s"
+    accuracy = clf.score(features_test,labels_test)
 
-
-accuracy = clf.score(features_test,labels_test)
-
-print("Accuracy: ", accuracy)
+    print "Accuracy with gamma= ", gamma, ", C= ",C," : ",round(accuracy,4)
 
 # checking if prediction is 1 (Chris)
-count = 0
-for i in range(0, len(pred)):
-    if pred[i] == 1:
-        count += 1
-print "Number of emails from chris: ", count
+# count = 0
+# for i in range(0, len(pred)):
+#     if pred[i] == 1:
+#         count += 1
+# print "Number of emails from chris: ", count
 #########################################################
