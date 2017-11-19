d903c7f6c6d49e94742c34335dbbe959b3814ebf
Suhail Ahamed <suhail511@gmail.com>
2017-11-19 18:11:09 +0400

    find the outlier with crazy salary

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -14,6 +14,24 @@ data = featureFormat(data_dict, features)
 
 
 ### your code below
+# for point in data:
+#     salary = point[0]
+#     bonus = point[1]
+#     matplotlib.pyplot.scatter( salary, bonus )
+#
+# matplotlib.pyplot.xlabel("salary")
+# matplotlib.pyplot.ylabel("bonus")
+# matplotlib.pyplot.show()
+
+i = -1
+for point in data_dict:
+    i += 1
+    if( data_dict[data_dict.keys()[i]]["salary"] != "NaN" and data_dict[data_dict.keys()[i]]["salary"] > 10000000 ):
+        print i , point, data_dict[data_dict.keys()[i]]["salary"]
 
 
-
+## print data_dict[data_dict.keys()[66]]
+# print data_dict[data_dict.keys()[67]]
+# print data_dict[data_dict.keys()[68]]
+# print data_dict[data_dict.keys()[69]]
+# print data_dict[data_dict.keys()[70]]
