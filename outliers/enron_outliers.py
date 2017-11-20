abb8b620b86a84c2a586e064baece9bd84183c08
Suhail Ahamed <suhail511@gmail.com>
2017-11-20 12:59:38 +0400

    find max/min exercised_stock_options

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -25,12 +25,20 @@ matplotlib.pyplot.ylabel("bonus")
 matplotlib.pyplot.show()
 
 i = -1
+max = 0
+min = 999999999
 for point in data_dict:
     i += 1
-    if( data_dict[data_dict.keys()[i]]["salary"] != "NaN" and data_dict[data_dict.keys()[i]]["salary"] > 1000000 and data_dict[data_dict.keys()[i]]["bonus"] > 5000000):
-        print i , point, data_dict[data_dict.keys()[i]]["salary"]
-
-
+    # if( data_dict[data_dict.keys()[i]]["salary"] != "NaN" and data_dict[data_dict.keys()[i]]["salary"] > 1000000 and data_dict[data_dict.keys()[i]]["bonus"] > 5000000):
+    #     print i , point, data_dict[data_dict.keys()[i]]["salary"]
+    if(data_dict[data_dict.keys()[i]]["exercised_stock_options"] > max and data_dict[data_dict.keys()[i]]["exercised_stock_options"] != "NaN"):
+        # print i , point, data_dict[data_dict.keys()[i]]["exercised_stock_options"]
+        max = data_dict[data_dict.keys()[i]]["exercised_stock_options"]
+    if(data_dict[data_dict.keys()[i]]["exercised_stock_options"] < min and data_dict[data_dict.keys()[i]]["exercised_stock_options"] != "NaN"):
+        # print i , point, data_dict[data_dict.keys()[i]]["exercised_stock_options"]
+        min = data_dict[data_dict.keys()[i]]["exercised_stock_options"]
+print "Max Value (exercised_stock_options): " , max
+print "Min Value (exercised_stock_options): " , min
 # print data_dict[data_dict.keys()[66]]
 # print data_dict[data_dict.keys()[67]]
 # print data_dict[data_dict.keys()[68]]
