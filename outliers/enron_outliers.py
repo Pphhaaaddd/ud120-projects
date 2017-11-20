b87a6db80ebb781f2743e6896710a52f353a6750
Suhail Ahamed <suhail511@gmail.com>
2017-11-20 13:05:27 +0400

    get min/max salary values

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -22,23 +22,37 @@ for point in data:
 
 matplotlib.pyplot.xlabel("salary")
 matplotlib.pyplot.ylabel("bonus")
-matplotlib.pyplot.show()
+# matplotlib.pyplot.show()
 
 i = -1
-max = 0
-min = 999999999
+max_stock = 0
+min_stock = 999999999
+max_sal = 0
+min_sal = 999999999
 for point in data_dict:
     i += 1
     # if( data_dict[data_dict.keys()[i]]["salary"] != "NaN" and data_dict[data_dict.keys()[i]]["salary"] > 1000000 and data_dict[data_dict.keys()[i]]["bonus"] > 5000000):
     #     print i , point, data_dict[data_dict.keys()[i]]["salary"]
-    if(data_dict[data_dict.keys()[i]]["exercised_stock_options"] > max and data_dict[data_dict.keys()[i]]["exercised_stock_options"] != "NaN"):
-        # print i , point, data_dict[data_dict.keys()[i]]["exercised_stock_options"]
-        max = data_dict[data_dict.keys()[i]]["exercised_stock_options"]
-    if(data_dict[data_dict.keys()[i]]["exercised_stock_options"] < min and data_dict[data_dict.keys()[i]]["exercised_stock_options"] != "NaN"):
-        # print i , point, data_dict[data_dict.keys()[i]]["exercised_stock_options"]
-        min = data_dict[data_dict.keys()[i]]["exercised_stock_options"]
-print "Max Value (exercised_stock_options): " , max
-print "Min Value (exercised_stock_options): " , min
+    if(data_dict[data_dict.keys()[i]]["exercised_stock_options"] != "NaN"):
+        if(data_dict[data_dict.keys()[i]]["exercised_stock_options"] > max_stock):
+            max_stock = data_dict[data_dict.keys()[i]]["exercised_stock_options"]
+        if(data_dict[data_dict.keys()[i]]["exercised_stock_options"] < min_stock):
+            min_stock = data_dict[data_dict.keys()[i]]["exercised_stock_options"]
+
+    if(data_dict[data_dict.keys()[i]]["salary"] != "NaN"):
+        if(data_dict[data_dict.keys()[i]]["salary"] > max_sal):
+            max_sal = data_dict[data_dict.keys()[i]]["salary"]
+        if(data_dict[data_dict.keys()[i]]["salary"] < min_sal):
+            min_sal = data_dict[data_dict.keys()[i]]["salary"]
+
+
+print "Max Value (exercised_stock_options): " , max_stock
+print "Min Value (exercised_stock_options): " , min_stock
+
+print "Max Value (salary): " , max_sal
+print "Min Value (salary): " , min_sal
+
+
 # print data_dict[data_dict.keys()[66]]
 # print data_dict[data_dict.keys()[67]]
 # print data_dict[data_dict.keys()[68]]
